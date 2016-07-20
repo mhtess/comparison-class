ca5dbb8ff3870887fcaea86c83a2ef52a4e758a0
lopez-brau <lopez_mic@hotmail.com>
2016-07-20 15:33:42 -0700

    renamed

---
+++ experiments/js/examples.js
@@ -0,0 +1,18 @@
+var examples = [
+	{
+		"The coffee is hot." : [
+			"John has never been to this coffee shop and buys a cup of coffee.",
+			"John is a regular at the coffee shop and buys a cup of coffee.",
+			"John takes a sip of his friend's coffee before returning back to his tea."
+		]
+	},
+	{
+		"It is cold." : [
+			"John just flew into New York from Florida."
+		]
+	},
+	{
+
+	}
+
+]
