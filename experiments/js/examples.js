82764b326b47b0efd1855a256b59eb34eab403ac
lopez-brau <lopez_mic@hotmail.com>
2016-07-21 12:21:02 -0700

    integrated examples into .js file

--- experiments/js/examples.js
+++ experiments/js/examples.js
@@ -1,18 +1,45 @@
-var examples = [
-	{
-		"The coffee is hot." : [
-			"John has never been to this coffee shop and buys a cup of coffee.",
-			"John is a regular at the coffee shop and buys a cup of coffee.",
-			"John takes a sip of his friend's coffee before returning back to his tea."
-		]
-	},
-	{
-		"It is cold." : [
-			"John just flew into New York from Florida."
-		]
-	},
-	{
-
-	}
-
-]
+var examples = {
+	"The coffee is hot." : [
+		"John has never been to this coffee shop and buys a cup of coffee.",
+		"John is a regular at the coffee shop and buys a cup of coffee.",
+		"John takes a sip of his friend's coffee before returning back to his tea."
+	],
+	"The weather is cold." : [
+		"John just flew into New York from Florida.",
+		"John is a New York native.",
+		"John is not wearing any warm clothing." 
+	],
+	"Susan is tall." : [
+		"Susan is a 4-year-old.",
+		"Susan is a jockey.",
+		"Susan is a giraffe."
+	],
+	"Bill is short." : [
+		"Bill is a goat.",
+		"Bill is a basketball player.",
+		"Bill is a tennis player."
+	],
+	"The necklace is expensive." : [
+		"John is at the jewelry store.",
+		"John is buying a diamond necklace.",
+		"John is buying his high-school prom date a necklace."
+	],
+	"The ticket was cheap." : [
+		"John bought a cruise ticket.",
+		"John bought a movie ticket.",
+		"John bought a plane ticket.",
+		"John got pulled over."
+	],
+	"Beth is heavy." : [
+		"Beth is a person.",
+		"Beth is a cow."
+	],
+	"Susan is light." : [
+		"Susan is a 4-year-old.",
+		"Susan is a dancer."
+	],
+	"Switzerland is far away." : [ 
+		"John lives in Germany.",
+		"John lives in the Caribbean."
+	]
+};
