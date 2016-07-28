3495a4e9368d4cb8400c980f3f6a08e6cfd63373
lopez-brau <lopez_mic@hotmail.com>
2016-07-28 01:23:11 -0700

    changed formatting

--- experiments/js/examples.js
+++ experiments/js/examples.js
@@ -1,59 +1,66 @@
-// examples stores the target sentences as keys (string literals) and the context sentences as an array of values for each key.
-var examples = {
-	"The coffee is hot" : [
+// examples stores the target sentences and an array of their corresponding context sentences
+var examples = [{
+	target : "The coffee is hot", 
+	context : [
 		// " has never been to this coffee shop and buys a cup of coffee.",
 		" is a regular at the coffee shop and buys a cup of coffee.",
 		" takes a sip of their friend's coffee before returning back to their tea."
-	],
-	"The weather is cold" : [
+	]
+}, {
+	target : "The weather is cold",
+	context : [
 		// " is not wearing a jacket or sweater.",
 		" just flew into New York from Florida.",
 		" is a New York native."
-	],
-	"The necklace is expensive" : [
+	]
+}, {
+	target : "The necklace is expensive",
+	context : [
 		// " is buying their high-school date a necklace.",
 		" is browsing at a jewelry store.",
 		" is buying a diamond necklace."
-	],
-	"The ticket is cheap" : [
+	]
+}, {
+	target : "The ticket is cheap",
+	context : [
 		// " is buying a cruise ticket.",
 		// " is buying a plane ticket.",
 		" is buying a movie ticket.",
 		" got pulled over by a police officer." // technically not proper English usage
-	], 
-	"Switzerland is far away" : [ 
+	]
+}, {
+	target : "Switzerland is far away",
+	context : [ 
 		" lives in Germany.",
 		" lives in the Caribbean."
-	],
-	"This is a long car ride" : [
+	]
+}, {
+	target : "This is a long car ride",
+	context : [
 		// " is driving to a theme park.",
 		" is driving across the country.",
 		" is driving to work."
-	],
-	"The crane is tall" : [
+	]
+}, {
+	target : "The crane is tall",
+	context : [
 		// " sees many kinds of animals at the city park.",
 		" is at a construction site.",
 		" is a veterinarian."
 	]
-};
-
-// additional examples with keys using dynamic literals
-examples[" is short"] = [
-	// " is a tennis player.",
-	" is a goat.",
-	" is a basketball player."
-];
-
-examples[" is heavy"] = [
-	// " is a cow.",
+}, {
+	target : " is short",
+	context : [
+		// " is a tennis player.",
+		" is a goat.",
+		" is a basketball player."
+	]
+}, {
+	target : " is heavy",
+	context : [
+		// " is a cow.",
 		// " is a featherweight boxer."
-	" is a person.",
-	" is a dancer."
-];
-
-// additional examples with repeated adjectives
-/*examples[" is tall"] = [
-	" is a 4-year-old.",
-	" is a jockey.",
-	" is a giraffe."
-];*/
+		" is a person.",
+		" is a dancer."
+	]
+}];
