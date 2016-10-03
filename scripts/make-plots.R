3e5fa8b2e6e5c9bd65a4a723af0c4570576d0d6f
lopez-brau <lopez_mic@hotmail.com>
2016-10-03 13:04:40 -0400

    processed raw responses with the caveman parser

--- scripts/make-plots.R
+++ scripts/make-plots.R
@@ -1,19 +1,25 @@
 # set the working directory to your local path to comparison-class
-path = "~/Desktop/comparison-class/"
+path = "~/cocolab/comparison-class/"
+
+# set the experiment title
+experiment = "pilot-1-paraphrase"
+
+# is the data processed by hand or through caveman?
+format = "processed"
 
 # import the ggplot2 library for plotting
 library(ggplot2)
 
 for (condition in c(1, 2)) {
   # set the working directory as the plots folder for the current condition
-  setwd(paste(path, "analysis/pilot-1-paraphrase/processed/pilot-1-paraphrase-trials-condition-", as.character(condition), "-plots/", sep = ""))
+  setwd(paste(path, "analysis/", experiment, "/", format, "/", experiment, "-trials-condition-", as.character(condition), "-plots/", sep = ""))
   
   # get the number of files in the current condition folder
-  files = list.files(paste(path, "data/pilot-1-paraphrase/processed/pilot-1-paraphrase-trials-condition-", as.character(condition), sep = ""))
+  files = list.files(paste(path, "data/", experiment, "/", format, "/", experiment, "-trials-condition-", as.character(condition), sep = ""))
   
   for (pair in 1:length(files)) {
     # read in the csv file, plot the data, and save it as a png image
-    file = read.csv(paste(path, "data/pilot-1-paraphrase/processed/pilot-1-paraphrase-trials-condition-", as.character(condition), "/pair-", as.character(pair) ,"-condition-", as.character(condition), sep = ""))
+    file = read.csv(paste(path, "data/", experiment, "/", format, "/", experiment, "-trials-condition-", as.character(condition), "/pair-", as.character(pair) ,"-condition-", as.character(condition), sep = ""))
     png(filename = paste("pair-", as.character(pair), "-condition-", as.character(condition), "-plot.png", sep = ""), width = 583, height = 380, units = "px", res = 125)
     build = ggplot_build(ggplot(data = file["response"], aes(x = file[,"response"])) + geom_bar())
     response = build$panel$ranges[[1]]$x.labels
