3f28bfe74114ae484f2cfb41f9d9cc9bcd566257
lopez-brau <lopez_mic@hotmail.com>
2016-10-04 19:10:08 -0400

    updated write-up and examples

--- experiments/js/examples.js
+++ experiments/js/examples.js
@@ -1,77 +1,57 @@
 // examples stores the target sentences and an array of their corresponding context sentences
 var examples = [{
-	target : "The coffee is hot", 
+	target : "The coffee is warm", 
 	context : [
 		// " has never been to this coffee shop and buys a cup of coffee.",
 		" is a regular at the coffee shop and buys a cup of coffee.",
-		" takes a sip of their friend\'s coffee before returning back to their tea."
+		" purchases an iced coffee."
 	]
 }, {
-	target : "The weather is cold",
+	target : "It's cold outside", // we can go back to the original version ("The weather is cold") but Turkers said it sounded off
 	context : [
-		// " is not wearing a jacket or sweater.",
-		// " is a New York native.",
-		" just flew into New York from Florida.",
+		" lives in Canada.",
 		" was raised in Russia."
 	]
 }, {
 	target : "The necklace is expensive",
 	context : [
-		// " is buying their high-school date a necklace.",
-		// " is trying to pawn their necklace.",
 		" is browsing at a jewelry store and stops to look at a necklace.",
 		" is buying a diamond necklace."
 	]
 }, {
-	target : "The ticket is cheap",
+	target : "The food is cheap ",
 	context : [
-		// " is buying a cruise ticket.",
-		// " is buying a plane ticket.",
-		" is buying a movie ticket.",
-		" got pulled over by a police officer." // technically not proper English usage
+		" is at a grocery store.",
+		" is at a seafood restaurant."
 	]
 }, {
 	target : "This is a long car ride",
 	context : [
-		// " is driving to a theme park.",
-		" is driving across the country.",
+		" is driving to a theme park.",
 		" is driving to work."
 	]
 }, {
-	target : "The office is open late",
+	target : "The office is open late today", // original version ("The office is open late")
 	context : [
 		" is making a doctor\'s appointment.",
-		" works at a call center."
+		" works at an elementary school."
 	]
 }, {
 	target : " is tall",
 	context : [
 		" is 4 years old.",
-		" is 26 years old."
+		" is from Mexico."
 	]
 }, {
 	target : " is short",
 	context : [
-		// " is a tennis player.",
-		" is a greyhound.",
+		" is 26 years old.",
 		" is a professional basketball player."
 	]
 }, {
 	target : " is heavy",
 	context : [
-		// " is a cow.",
-		// " is a featherweight boxer."
 		" is a person.",
 		" is a dancer."
 	]
 }];
-
-// {
-// 	target : "Switzerland is far",
-// 	context : [ 
-// 		// " lives in Germany.",
-// 		// " lives in the Caribbean.",
-// 		" lives 30 miles away from Switzerland and is planning a vacation.",
-// 		" lives 10000 miles away from Switzerland and is planning a vacation."
-// 	]
-// } 
