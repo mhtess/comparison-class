09a0bab8728c8208c54a4f03d8bc713c101b7754
lopez-brau <lopez_mic@hotmail.com>
2016-10-10 00:44:37 -0400

    added word occurrence chart

--- scripts/count-words.R
+++ scripts/count-words.R
@@ -1,23 +1,24 @@
 # set the working directory to your local path to comparison-class
-path = "~/cocolab/comparison-class/"
+path = "~/cocolab/comparison-class/data/pilot-1-paraphrase/raw/pilot-1-paraphrase-trials-condition-2/"
 
-# set the working directory to the path we are interested in
-setwd(paste(path, "data/pilot-1-paraphrase/raw/pilot-1-paraphrase-trials-condition-2", sep = ""))
-
-# store the files in the current directory
-files = list.files(".")
+# list the files in the current directory
+files = list.files(path)
 
 # stores the number of times "other" or "typical" has appeared in a file
-occ_other = 0
-occ_typical = 0
+other = 0
+typical = 0
 
-# a chart that stores the information we need
-chart = data.frame(freq_other = 1:length(files), freq_typical = 1:length(files), occ_other = 1:length(files), occ_typical = 1:length(files), responses = 1:length(files))
+# create a chart that stores the information we need
+chart = data.frame("other.count" = 1:length(files),
+                   "typical.count" = 1:length(files),
+                   "other.frequency" = 1:length(files),
+                   "typical.frequency" = 1:length(files),
+                   "responses.total" = 1:length(files))
 
 # iterate through each file and check for the occurrence of "other" and "typical"
 for (f in 1:length(files)) {
   # read in the current file
-  file = read.csv(paste("pair-", f, "-condition-2", sep = ""))
+  file = read.csv(paste(path, "pair-", f, "-condition-2", sep = ""))
 
   # remove levels and isolate the responses
   responses = as.character(file[,"response"])
@@ -26,22 +27,22 @@ for (f in 1:length(files)) {
   for (r in 1:length(responses)) {
     # if "other" is found within the current response, increment the counter
     if (grepl("other", responses[r])) {
-      occ_other = occ_other + 1
+      other = other + 1
     }
     # else, if "typical" is found within the current response, increment the counter
     else if (grepl("typical", responses[r])) {
-      occ_typical = occ_typical + 1  
+      typical = typical + 1  
     }
   }
   
   # store the frequency of occurence, the occurrence, and the total number of responses of the current file
-  chart[,"freq_other"][f] = occ_other / length(responses)
-  chart[,"freq_typical"][f] = occ_typical / length(responses)
-  chart[,"occ_other"][f] = occ_other
-  chart[,"occ_typical"][f] = occ_typical
-  chart[,"responses"][f] = length(responses)
+  chart[,"other.count"][f] = other
+  chart[,"typical.count"][f] = typical
+  chart[,"other.frequency"][f] = other / length(responses)
+  chart[,"typical.frequency"][f] = typical / length(responses)
+  chart[,"responses.total"][f] = length(responses)
   
   # reset the occurrence counter
-  occ_other = 0
-  occ_typical = 0
+  other = 0
+  typical = 0
 }
