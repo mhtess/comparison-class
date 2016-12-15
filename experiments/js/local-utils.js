33857df35c2355bf6787f6bc8014cdffa2e1c90c
Michael Lopez-Brau <lopez_mic@hotmail.com>
2016-12-15 03:57:43 -0500

    added unit options to the experiment and updated experiment prompts

--- experiments/js/local-utils.js
+++ experiments/js/local-utils.js
@@ -5,7 +5,9 @@ function getTrials(examples) {
 		for (var j = 0; j < examples[i].context.length; j++) {
 			trials.push({
 				target : examples[i].target,
-				context : examples[i].context[j]
+				context : examples[i].context[j],
+        degree : examples[i].degree,
+        unit : examples[i].unit
 			});
 		}
 	}
@@ -14,7 +16,7 @@ function getTrials(examples) {
 
 // samples without replacement from a list of names for all of our trial slides
 // NOTE: 30 NAMES MAX
-var sampleNames = function(characters, trials) {
+var sampleNames = function(characters) {
 	var names = _.pluck(characters, "name");
 	return _.shuffle(names);
 }
@@ -30,7 +32,7 @@ var getPronoun = function(context, name) {
 
 // sample a condition, where a condition is the use of the "for a" or "relative to"
 var sampleCondition = function() {
-	return " for a " //_.sample([" for a ", " relative to "]);
+	return " for a "; //_.sample([" for a ", " relative to "]);
 }
 
 // embeds the trial slides that were generated in the experiment file into the html file
@@ -49,25 +51,35 @@ function embedSlides(trials) {
   }
 }
 
-function embedListenerSlides(trials) {
+// embeds the trial slides that were generated in the experiment file into the html file for the listener experiment
+function embedListenerSlides(examples, trials) {
+  // get the units 
+  var unit = _.pluck(examples, "unit");
   var slides = "";
   for (var i = 1; i <= trials; i++) {
+    var u = "";
+    // generate the script for the units
+    for (var j = 0; j < unit[i-1].length; j++) {
+      u = u + "<label><option value=\"" + unit[i-1][j] + "\">" + unit[i-1][j] + "</option></label>";
+    }
     slides = slides + "<div class=\"slide\" id=\"trial" + i + "\">" +
     	"<p class=\"display_context\"></p>" +
   		"<p class=\"display_target\"></p>" +
   		"<p class=\"display_question\"></p>" +
-    "<input type=\"text\" id=\"measure" + i + "\" maxlength=\"5\" size =\"5\" tabindex=\"1\"> </input>"+
-    "<select id=\"unit"+ i +"\">"+
-	" <label><option value=\"F\">degrees Fahrenheit</option></label>"+
-   "<label><option value=\"C\" >degrees Celsius</option></label>"+
-     "</select><br><br>" +
-			// "<p class=\"slider_number\"></p>" +
-			// " <table id=\"slider_table\"class=\"slider_table\">"+
-			// "<tr><td class=\"left\"></td><td class=\"right\"></td></tr>"+
-			// "<tr><td colspan=\"2\"><div id=\"single_slider\"class=\"slider\"></div></td></tr>"+
-			// "</table>" +
+      "<input type=\"text\" id=\"measure" + i + "\" maxlength=\"5\" size =\"5\" tabindex=\"1\"></input>" +
+      "<select id=\"unit" + i + "\">" + 
+      "<option selected disabled hidden style='display: none' value=''></option>" + 
+      u + 
+      "</select><br><br>" +
+  		// "<p class=\"slider_number\"></p>" +
+  		// " <table id=\"slider_table\"class=\"slider_table\">" +
+  		// "<tr><td class=\"left\"></td><td class=\"right\"></td></tr>" +
+  		// "<tr><td colspan=\"2\"><div id=\"single_slider\"class=\"slider\"></div></td></tr>" +
+  		// "</table>" +
   		"<button onclick=\"_s.button()\">Continue</button>" +
-  		"<p class=\"err\">Please write something.</p>" +
+      "<p class=\"err\">Please write something and select a unit.</p>" + 
+  		"<p class=\"responseErr\">Please write something.</p>" +
+      "<p class=\"unitErr\">Please select a unit.</p>" +
   		"</div>";
   	$(".trial_slides").html(slides);
   }
