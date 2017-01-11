436d49f0cf75bbdc0d473bb2fc1214732d678879
Michael Lopez-Brau <lopez_mic@hotmail.com>
2017-01-10 23:22:21 -0500

    storing catch trial results

--- experiments/js/vague-prior-elicitation-number.js
+++ experiments/js/vague-prior-elicitation-number.js
@@ -14,6 +14,7 @@ function makeSlides(f) {
   slides.instructions = slide({
     name : "instructions",
     button : function() {
+      exp.catch_trials.push($("#catch").val());
       exp.go(); // use exp.go() if and only if there is no "present" data
     }
   });
@@ -163,7 +164,7 @@ function init() {
   // the blocks of the experiment
   exp.structure = ["i0", "instructions"];
   for (var k = 1; k <= exp.trials; k++) {
-    exp.structure.push("trial" + k);
+    // exp.structure.push("trial" + k);
   }
   exp.structure.push("subj_info");
   exp.structure.push("thanks");
