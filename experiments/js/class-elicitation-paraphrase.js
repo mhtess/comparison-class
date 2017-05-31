00fbadc4d58d5d86cbc49d536e27f1bec8fb8611
Michael Lopez-Brau <lopez_mic@hotmail.com>
2017-05-31 17:20:48 -0400

    storing catch trial response

--- experiments/js/class-elicitation-paraphrase.js
+++ experiments/js/class-elicitation-paraphrase.js
@@ -25,6 +25,7 @@ function makeSlides(f) {
         $(".catchErr").show();
       }
       else {
+        exp.catch_trial = catch_response;
         exp.go();
       }
     }
@@ -151,7 +152,7 @@ function makeSlides(f) {
     start: function() {
       exp.data = {
           "trials": exp.data_trials,
-          "catch_trials": exp.catch_trials,
+          "catch_trial": exp.catch_trial,
           "system": exp.system,
           "condition": exp.condition,
           "paraphrase0": exp.sliderOrder[0],
@@ -208,7 +209,7 @@ function init() {
 
   // stores the catch trial results for this experiment
   exp.nCatch = 2;
-  exp.catch_trials = [];
+  exp.catch_trial = [];
 
   // get user system specs
   exp.system = {
