21a346c1ee1bed7bbcd4e4cb01dc7e3ca8dbb2ed
Brin Harper <brin.harper.2017@gmail.com>
2019-05-15 09:07:12 -0400

    Sorting trial data

--- analysis/pilot-classElicitation-3-analysis.py
+++ analysis/pilot-classElicitation-3-analysis.py
@@ -1,5 +1,6 @@
 from nltk.corpus import wordnet as wn
 import csv
+import json
 
 # np = "pork"
 # class_elicited_1 = "meats"
@@ -15,9 +16,13 @@ import csv
 # print(np in np_synset.lemma_names())
 # print(class_elicited_1_synset in np_hypernyms)
 
+# Constants -- edit these if you change the experiment structure!
 NUM_WORKERS = 81
 NUM_MEMCHECK_TRIALS = 10
 MEMCHECK_TOLERANCE = 2
+NUM_DATA_TRIALS = 18
+
+# Identify workers who failed the memory check
 
 memory_check = csv.reader(open("../data/pilot-classElicitation-free-3/class-elicitation-free-3-catch_trials.csv"))
 
@@ -37,4 +42,37 @@ for worker_id in range(NUM_WORKERS):
     if num_wrong > MEMCHECK_TOLERANCE:
         failed_memcheck.append(worker_id)
 
-print(failed_memcheck)
+print(str(len(failed_memcheck)) + " workers failed the memory check.")
+
+
+# Read in and sort all trial data, ignoring workers who failed the memory check
+
+trial_data = csv.reader(open("../data/pilot-classElicitation-free-3/class-elicitation-free-3-trials.csv"))
+i = iter(trial_data)
+trial_data_headers = i.__next__()
+print(trial_data_headers)
+worker_id = trial_data_headers.index('workerid')
+np_index = trial_data_headers.index('np')
+adj_index = trial_data_headers.index('adj')
+np_positiveness_index = trial_data_headers.index('np_positiveness')
+adj_positiveness_index = trial_data_headers.index('adj_positiveness')
+response_index = trial_data_headers.index('response')
+
+# Build structure to hold trial data together for each of the 20 variations
+
+all_trial_data = dict()
+
+with open("../experiments/js/noun_elicitation_pilot.json") as json_file:
+    stimuli = iter(json.load(json_file)["examples"])
+    for stim in stimuli:
+        for np_option in ['positive', 'neither_nor', 'negative']:
+            for adj_option in ['adj_positive', 'adj_negative']:
+                pass
+
+for row in i:
+    if not int(row[worker_id]) in failed_memcheck:
+        np = row[np_index]
+        adj = row[adj_index]
+        np_positiveness = row[np_positiveness_index]
+        adj_positiveness = row[adj_positiveness_index]
+        response = row['response']
