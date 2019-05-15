a1b895d100e4574b1c7f6b7aa0b5a78b406368c0
Brin Harper <brin.harper.2017@gmail.com>
2019-05-15 08:12:50 -0400

    Identifying memcheck failures

--- analysis/pilot-classElicitation-3-analysis.py
+++ analysis/pilot-classElicitation-3-analysis.py
@@ -1,15 +1,40 @@
 from nltk.corpus import wordnet as wn
+import csv
 
-np = "pork"
-class_elicited_1 = "meats"
+# np = "pork"
+# class_elicited_1 = "meats"
+#
+# np_synset = wn.synsets(np)[0]
+# np_hypernyms = set([i for i in np_synset.closure(lambda s:s.hypernyms())])
+# class_elicited_1_synset = wn.synsets(class_elicited_1)[0]
+#
+# print(np_synset)
+# print(class_elicited_1_synset)
+# print(np_hypernyms)
+#
+# print(np in np_synset.lemma_names())
+# print(class_elicited_1_synset in np_hypernyms)
 
-np_synset = wn.synsets(np)[0]
-np_hypernyms = set([i for i in np_synset.closure(lambda s:s.hypernyms())])
-class_elicited_1_synset = wn.synsets(class_elicited_1)[0]
+NUM_WORKERS = 81
+NUM_MEMCHECK_TRIALS = 10
+MEMCHECK_TOLERANCE = 2
 
-print(np_synset)
-print(class_elicited_1_synset)
-print(np_hypernyms)
+memory_check = csv.reader(open("../data/pilot-classElicitation-free-3/class-elicitation-free-3-catch_trials.csv"))
 
-print(np in np_synset.lemma_names())
-print(class_elicited_1_synset in np_hypernyms)
+i = iter(memory_check)
+memory_check_headers = i.__next__()
+workerid = memory_check_headers.index('workerid')
+tested_on = memory_check_headers.index('tested_on')
+response = memory_check_headers.index('response')
+
+failed_memcheck = []
+
+for worker_id in range(NUM_WORKERS):
+    num_wrong = 0
+    for trial in range(NUM_MEMCHECK_TRIALS):
+        row = i.__next__()
+        num_wrong += not (row[tested_on] == row[response])
+    if num_wrong > MEMCHECK_TOLERANCE:
+        failed_memcheck.append(worker_id)
+
+print(failed_memcheck)
