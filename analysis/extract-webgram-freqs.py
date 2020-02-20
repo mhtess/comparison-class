eba00002147cf3368ec26ea94d8f6e54371e1fcd
polina-tsvilodub <42241414+polina-tsvilodub@users.noreply.github.com>
2020-02-20 08:25:15 -0500

    df with prodeced superordinates w freqs

--- analysis/extract-webgram-freqs.py
+++ analysis/extract-webgram-freqs.py
@@ -7,16 +7,16 @@ from nltk.tokenize import WhitespaceTokenizer
 base_url = "https://corpora.linguistik.uni-erlangen.de/cgi-bin/demos/Web1T5/"
 
 # read in unique responses
-data_path = '../data/class-elicitation-prereg-final/class-elicitation-final-full_analysis_w_pl_np.csv'
+data_path = '../data/class-elicitation-prereg-final/cc-prod-distinct-super-pl.csv'
 data = pd.read_csv(data_path)
 
 # extract unique superordinates
-supers = data['superordinate.freq'].drop_duplicates().tolist()
+supers = data['produced_super_pl'].drop_duplicates().tolist()
 # extract unique modal responses
-modes = data['np_pl'].drop_duplicates().tolist()
+# modes = data['np_pl'].drop_duplicates().tolist()
 # all responses
-unique_nps_pl = list(set(supers + modes ))
-
+# unique_nps_pl = list(set(supers + modes ))
+unique_nps_pl = list(set(supers ))
 lemmatizer = WordNetLemmatizer()
 
 # super_sg = data['superordinate'].drop_duplicates().apply(lambda word: lemmatizer.lemmatize(word)).tolist()
@@ -54,7 +54,7 @@ for np in unique_nps_pl:
 
 # create DF of frequencies and words
 df_nps = pd.DataFrame(list(zip(words_pl, freqs_pl)), columns=['NPs', 'Frequencies'])
-df_nps.to_csv('../data/class-elicitation-prereg-final/class-elicitation-final-plural_frequencies.csv')
+df_nps.to_csv('../data/class-elicitation-prereg-final/cc-prod-produced-super-pl-frequencies.csv')
 
 # for np in unique_nps_sg:
 #     webgram_page = requests.get(base_url + "Web1T5_freq.perl?query=" + np + "&mode=Search&limit=50&threshold=100&optimize=on&wildcards=listed+normally&fixed=shown&.cgifields=optimize&.cgifields=debug")
